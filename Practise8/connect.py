f69b028d9d18262185756ac79ed9edba7b0ca0b5
Ayau <aaulymkhantarbaj@gmail.com>
2026-04-13 21:45:44 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

--- Practise8/connect.py
+++ Practise8/connect.py
@@ -1,11 +1,18 @@
 import psycopg2
-from config import DB_CONFIG
+from config import load_config
 
-def get_connection():
+def connect(config):
+    """ Connect to the PostgreSQL database server """
     try:
-        conn = psycopg2.connect(**DB_CONFIG)
-        return conn
-    except Exception as e:
-        print("Connection error:", e)
-        return None
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as conn:
+            print('Connected to the PostgreSQL server.')
+            return conn
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    config = load_config()
+    connect(config)
     
--- Practise8/functions.sql
+++ Practise8/functions.sql
@@ -1,13 +1,24 @@
--- functions.sql
-CREATE OR REPLACE FUNCTION search_contacts(p_pattern TEXT)
-RETURNS TABLE(name TEXT, phone TEXT)
-LANGUAGE plpgsql
-AS $$
+-- A function that returns all records matching a pattern (part of name, surname, or phone number)
+
+CREATE OR REPLACE FUNCTION records(p text)
+RETURNS TABLE(out_id INTEGER, out_name VARCHAR, out_phone VARCHAR) AS $$
 BEGIN
     RETURN QUERY
-    SELECT c.name, c.phone
-    FROM contacts AS c
-    WHERE c.name ILIKE '%' || p_pattern || '%'
-       OR c.phone ILIKE '%' || p_pattern || '%';
+        SELECT id, username, phone FROM phonebook 
+        WHERE username ILIKE '%' || p || '%'
+        OR phone ILIKE '%' || p || '%';
 END;
-$$;
+$$ LANGUAGE plpgsql;
+
+-- A function that queries data from the table with pagination (by LIMIT and OFFSET)
+
+CREATE OR REPLACE FUNCTION pagination(lim int, offs int)
+RETURNS TABLE(out_id INTEGER, out_name VARCHAR, out_phone VARCHAR) AS $$
+BEGIN
+    RETURN QUERY
+        SELECT id, username, phone
+        FROM phonebook
+        ORDER BY username
+        LIMIT lim OFFSET offs;
+END;
+$$ LANGUAGE plpgsql;
